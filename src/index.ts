a784267396b942e5a068e8a71e8477695056073f
Gavin M <gavin.miller@sinnerschrader.com>
2025-04-04 10:02:03 +0200

    feat: add createGenesisBlock fn
    This commit uses Node's createHash function using "sha256" as the
    encryption algorithm.
    
    We use 'hex' as the hash.digest() BinaryToTextEncoding arg for several reasons, including;
    - case insensitive
    - consistent length
    - no special characters

--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,71 @@
-import * as readline from 'node:readline';
+import * as readline from "node:readline";
+import fs from "node:fs";
+
+const { createHash } = await import("node:crypto");
+
+const blockchainJson = fs.readFileSync("./blockchain.json", {
+  encoding: "utf8",
+});
+const parsedBlockchain: Blockchain = JSON.parse(blockchainJson);
+const blockchain = parsedBlockchain.blockchain;
+
+interface Blockchain {
+  blockchain: Block[];
+}
+
+interface GenesisBlock {
+  blockNumber: number;
+  timestamp: number;
+  hash: string | null;
+}
 
 interface Block {
-  blockNumber: number;  
-  timestamp: string;  
-  prevHash: string | null;  
-  hash: string | null;  
-};
-
-type Blockchain = Block[];
+  blockNumber: number;
+  timestamp: number;
+  prevHash: string;
+  hash: string | null;
+}
 
 interface NewEntryData {
   prevHash: string | null;
-  prevBlockNumber: number;
-};
+  prevBlockNumber: number | null;
+}
 
-//console.log("Programming running..");
+async function createGenesisBlock(): Promise<GenesisBlock> {
+  const genesisBlock: GenesisBlock = {
+    blockNumber: 1,
+    timestamp: Date.now(), // leave this for now. For searching by date we could write a function that parses it
+    hash: null,
+  };
 
-// user input
-let entryInput = '';
+  const hashedBlock: string = await hashBlock(genesisBlock);
+  return { ...genesisBlock, hash: hashedBlock };
+}
 
+async function hashBlock(block: Block | GenesisBlock): Promise<string> {
+  let hash = createHash("sha256").update(block.toString());
+
+  return hash.digest("hex");
+}
+
+let entryInput = "";
+
+// get entry text
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-rl.question(`Add new journal entry:`, entry => {
-  
- entryInput = entry;
-  console.log("Entry saved");
+rl.question(`Add new journal entry:`, (entry) => {
+  entryInput = entry;
+  console.log(`Registered entry: "${entry.trim()}"`);
+
   rl.close();
 });
 
+let genesis: GenesisBlock;
 
-
-
-
-function createBlock(data: NewEntryData): Block {
-
- const newBlock: Block = {
-   blockNumber: data.prevBlockNumber += 1,
-   timestamp: Date.now().toString(), 
-   hash: null,
-   prevHash: data.prevHash
- };
- 
- return newBlock;
-};
-
+if (blockchain.length === 0) {
+  genesis = await createGenesisBlock();
+  console.log("final genesis block", genesis);
+}
